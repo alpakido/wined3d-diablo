cde7e9c89a8945dca6a40cfbe645be3556398376
Yana Timoshenko <yana@riseup.net>
2021-11-06 23:47:54 +0000

    Output Wine version to $out/wine_version

--- wined3d.nix
+++ wined3d.nix
@@ -1,12 +1,13 @@
 { wine }:
 
 wine.overrideDerivation (old: {
-  name = "wined3d.dll";
+  name = "wined3d-diablo";
 
   makeFlags = [ "-C" "dlls/wined3d" ];
 
   installPhase = ''
     install -D dlls/wined3d/wined3d.dll.so $out/dlls/wined3d.dll
+    echo ${old.name} > $out/wine_version
   '';
 
   patches = [ ./ddraw_hack.patch ];
